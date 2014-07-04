ac4892d394d093c609f2237f1493972101495f00
JoeyKL <joey@kellison-linn.org>
2014-07-04 15:11:11 -0700

    Fix entities constructors so they actually return

--- Particle Accelerator/src/entity.js
+++ Particle Accelerator/src/entity.js
@@ -6,7 +6,7 @@
  * @param {number} h - The height of the rectangle
  */
 function newRectEntity(x, y, w, h) {
-    var ret = {
+    return {
         x: x,
         y: y,
         w: w,
@@ -27,19 +27,34 @@ function newRectEntity(x, y, w, h) {
  * @param {number} r - The circle radius
  */
 function newCircleEntity(x, y, r) {
-    var ret = {
-        x: x,
-        y: y,
-        r: r,
+    return {
+        position: {x: x, y: y},
+        velocity: {dx: 0, dy: 0},
+        
+        radius: r,
         
         render: function(ctx) {
             ctx.fillStyle = this.color || 'black';
             
             ctx.beginPath();
-            ctx.moveTo(x,y);
-            ctx.arc(x, y, r, 0, Math.PI * 2);
+            ctx.moveTo(this.position.x, this.position.y);
+            ctx.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2);
             ctx.closePath();
             ctx.fill();
+        },
+        
+        update: function(deltaTime) {
+            this.velocity.dy += 0.001;
+            updatePosition.call(this, deltaTime);
+            if(this.position.x<0) this.position.x += WIDTH;
+            if(this.position.x>=WIDTH) this.position.x -= WIDTH;
+            if(this.position.y<0) this.position.x += HEIGHT;
+            if(this.position.y>=HEIGHT) this.position.x -= HEIGHT;
         }
-    }
+    };
+}
+
+function updatePosition(deltaTime) {
+    this.position.x += this.velocity.dx*deltaTime;
+    this.position.y += this.velocity.dy*deltaTime;
 }
