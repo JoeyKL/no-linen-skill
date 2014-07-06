ee9794e3d5dc9144f5e4d8eb065d5249aded0350
JoeyKL <joey@kellison-linn.org>
2014-07-06 09:51:09 -0700

    Add mouse input

--- Particle Accelerator/src/game.js
+++ Particle Accelerator/src/game.js
@@ -11,18 +11,22 @@ var done = false;
 var then = Date.now();
 var now = Date.now();
 
+var mouse = {x: 0, y:0, down: false};
+
 function main() {
     init();
     mainLoop();
 }
 
 function init() {
-    
+    canvas.addEventListener("mousemove", function(e){mouse.x=e.pageX;mouse.y=e.pageY}, false);
+    canvas.addEventListener("mousedown", function(e){mouse.down = true}, false);
+    canvas.addEventListener("mouseup", function(e){mouse.down = false}, false);
 }
 
 function mainLoop() {
     now = Date.now();
-        
+    
     update(now - then);
     render(ctx);
     then = now;
